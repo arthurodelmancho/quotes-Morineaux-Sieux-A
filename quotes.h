d6d552eab4fa6b61c6ee9b7d0f056387de4573d4
julie <julie.sieux@efrei.net>
2025-03-26 17:51:38 +0100

    Initial quotes program

---
+++ quotes.h
@@ -0,0 +1,17 @@
+//
+// Created by julie on 26/03/2025.
+//
+
+#ifndef QUOTES_H
+ #define QUOTES_H
+
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
+
+ #define MAX_QUOTES 10
+
+ void print_quote(char ** quotes, int index);
+void print_random_quote(char ** quotes);
+
+#endif
